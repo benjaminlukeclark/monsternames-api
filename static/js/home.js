0d59b98627095476ec304f6f777168b89ca68dc4
Ben Clark <bclark@sudoblark.com>
2020-02-10 01:16:49 +0000

    Endpoints page content done
    -Finshed enpoints page content
    
    -Need to do contribute page content
    
    -Once all content is done then need to do styling

--- static/js/home.js
+++ static/js/home.js
@@ -2,17 +2,27 @@
 function apiGet() {
     // get url from page
     let url = document.getElementById('endpoint').innerHTML
+    console.log(url)
     fetch(url)
     .then((resp) => resp.json()) // Transform the data into json
     .then(function(data) {
-      console.log(data)
+      let dataJson = JSON.stringify(data, null, 4)
+      document.getElementById("apiGETResult").innerHTML = dataJson
       })
     .catch(function(err) {
-      let errorDict = {"firstName" : "ben", "lastName" : "clark", "fullName" : "ben clark"}
-      let errorJson = JSON.stringify(errorDict)
-      document.getElementById('endpoint').innerHTML = errorJson
-      console.log(errorJson)
+      let errorDict = {"error" : "Unable to query API", "errorDetails" : {} }
+      errorDict["errorDetails"] = {
+        "exception" : err.name,
+        "message" : err.message
+      }
+      console.log(errorDict)
+      let errorJson = JSON.stringify(errorDict, null, 4)
+      document.getElementById("apiGETResult").innerHTML = errorJson
     })
 
     return false // done to prevent reload of page
+}
+
+window.onload = function() {
+  document.getElementById("apiGET").onclick = apiGet()
 }
