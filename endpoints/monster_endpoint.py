37f35a1aa203f36a11b390864eea120dce979bdc
Sudoblark <bclark@sudoblark.com>
2020-02-25 17:31:51 +0000

    Changed string formats
    - Changed string formats

--- endpoints/monster_endpoint.py
+++ endpoints/monster_endpoint.py
@@ -12,8 +12,8 @@ class monster_endpoint:
         self._First_Name = self.__verify_param(First_Name_Present, bool)
         self._Last_Name = self.__verify_param(Last_Name_Present, bool)
         self._Monster_Name = self.__verify_param(Monster_Name, str)
-        self._First_Name_Model = self.__verify_model(First_Name_Model, "{self._Monter_Name} first name model")
-        self._Last_Name_Model = self.__verify_model(Last_Name_Model, "{self._Monter_Name} last name model")
+        self._First_Name_Model = self.__verify_model(First_Name_Model, f"{self._Monster_Name} first name model")
+        self._Last_Name_Model = self.__verify_model(Last_Name_Model, f"{self._Monster_Name} last name model")
     # Return name method
     def return_name(self):
         # Connect to db
@@ -47,7 +47,7 @@ class monster_endpoint:
             return make_response(jsonify({'error' : 'Duplicate record', 'message' : 'Record already exists'}), 400)
         # Insert
         self._First_Name_Model.create(firstName=firstName)
-        models.PostAudit.create(user=user, message='{self._Monster_Name} first name record "{0}" created'.format(firstName))
+        models.PostAudit.create(user=user, message=f"{self._Monster_Name} first name record '{firstName}' created")
         models.db.commit()
         models.db.close()
         # Return success
@@ -62,7 +62,7 @@ class monster_endpoint:
             return make_response(jsonify({'error' : 'Duplicate record', 'message' : 'Record already exists'}), 400)
         # Insert
         self._Last_Name_Model.create(lastName=lastName)
-        models.PostAudit.create(user=user, message='{self._Monster_Name} last name record "{0}" created'.format(lastName))
+        models.PostAudit.create(user=user, message=f"{self._Monster_Name} last name record '{lastName}' created")
         models.db.commit()
         models.db.close()
         # Return success
@@ -71,13 +71,13 @@ class monster_endpoint:
     # Method to verify that provided param is of the expected type
     def __verify_param(self, var, expected_type):
         if (isinstance(var, expected_type)) == False:
-            raise TypeError("{var} is not of expected type {expected_type}")
+            raise TypeError(f"{var} is not of expected type {expected_type}")
         else:
             return var
     # Method to verify that a valid model has been passed through
     def __verify_model(self, model, model_name):
         if (((isinstance(model, peewee.Model)) == False) & ((isinstance(model, None)) == False)):
-            raise TypeError("{model_name} provided is invalid. Must either be of type peewee.Model or None.")
+            raise TypeError(f"{model_name} provided is invalid. Must either be of type peewee.Model or None.")
         else:
             return model
 
