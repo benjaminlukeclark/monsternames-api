337e2014649539754d166a0d4a644d064cff5163
Sudoblark <bclark@sudoblark.com>
2020-06-19 02:29:55 +0100

    Ameded string format
    - Amended string formatting as was causing exceptions on dev server
    - Will commit then deploy to dev and test

--- endpoints/monster_endpoints.py
+++ endpoints/monster_endpoints.py
@@ -14,8 +14,8 @@ class monster_endpoint:
         self._First_Name = self.__verify_param(First_Name_Present, True)
         self._Last_Name = self.__verify_param(Last_Name_Present, True)
         self._Monster_Name = self.__verify_param(Monster_Name, "String")
-        self._First_Name_Model = self.__verify_model(First_Name_Model, f"{self._Monster_Name} first name model")
-        self._Last_Name_Model = self.__verify_model(Last_Name_Model, f"{self._Monster_Name} last name model")
+        self._First_Name_Model = self.__verify_model(First_Name_Model, "{fMonsterName} first name model".format(fMonsterName = self.Monster_Name))
+        self._Last_Name_Model = self.__verify_model(Last_Name_Model, "{fMonsterName} last name model".format(fMonsterName = self.Monster_Name))
     # Return name method
     def return_name(self):
         # Connect to db
@@ -60,7 +60,7 @@ class monster_endpoint:
             return make_response(jsonify({'error' : 'Duplicate record', 'message' : 'Record already exists'}), 400)
         # Insert
         self._First_Name_Model.create(firstName=firstName)
-        models.PostAudit.create(user=user, message=f"{self._Monster_Name} first name record '{firstName}' created")
+        models.PostAudit.create(user=user, message="{fMonsterName} first name record '{fFirstName}' created".format(fMonsterName = self.Monster_Name, fFirstName = firstName))
         models.db.commit()
         models.db.close()
         # Return success
@@ -84,7 +84,7 @@ class monster_endpoint:
             return make_response(jsonify({'error' : 'Duplicate record', 'message' : 'Record already exists'}), 400)
         # Insert
         self._Last_Name_Model.create(lastName=lastName)
-        models.PostAudit.create(user=user, message=f"{self._Monster_Name} last name record '{lastName}' created")
+        models.PostAudit.create(user=user, message="{fMonsterName} firstlast name record '{fLastName}' created".format(fMonsterName = self.Monster_Name, fLastName = lastName))
         models.db.commit()
         models.db.close()
         # Return success
@@ -93,14 +93,14 @@ class monster_endpoint:
     # Method to verify that provided param is of the expected type
     def __verify_param(self, var, expected_type):
         if (isinstance(var, type(expected_type))) == False:
-            raise TypeError(f"{var} is not of expected type {expected_type}")
+            raise TypeError("{fVar} is not of expected type {fExpectedType}".format(fVar = var, fExpectedType = expected_type))
         else:
             return var
     # Method to verify that a valid model has been passed through
     def __verify_model(self, model, model_name):
         model_type = type(model)
         if model_type == peewee.Model:
-            raise TypeError(f"{model_name} provided is invalid. Must either be of type peewee.Model or None.")
+            raise TypeError("{fModelName} provided is invalid. Must either be of type peewee.Model or None.".format(fModelName = model_name))
         else:
             return model
     # Method in order to verify that the user is authorised to make a POST request
